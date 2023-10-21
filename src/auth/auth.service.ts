ff39718c0b0307858816c490e9fa379ac0640e70
Victor Gabriel Vieira Francisco <victorgabrielfrancisco@gmail.com>
2023-10-21 13:52:31 -0300

    feat(auth): implement local strategy validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      //validacao de senha por hash
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+    // nao encontrou o user e/ou senha invalida
+    throw new Error('Email address or password provided is incorrect.');
   }
 }
