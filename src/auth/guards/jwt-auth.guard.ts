3bda43b5cfae18376db33a6e4f6f48f96179cd6f
Victor Gabriel Vieira Francisco <victorgabrielfrancisco@gmail.com>
2023-10-23 22:38:31 -0300

    feat(auth): correct

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -9,8 +9,8 @@ import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 // Decorators
 import { IS_PUBLIC_KEY } from '../decorators/is-public.decorator';
+import { UnauthorizedError } from 'src/errors/unauthorized.error';
 // Error Handling
-import { UnauthorizedError } from '../errors/unauthorized.error';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
